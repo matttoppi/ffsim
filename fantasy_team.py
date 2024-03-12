1610beeadc4ff22183bee3c785ad3fcb6a89b201
matttoppi <47908195+matttoppi@users.noreply.github.com>
2024-03-12 19:26:22 -0400

    created player and fantasyteam stubs

---
+++ fantasy_team.py
@@ -0,0 +1,23 @@
+
+
+
+class FantasyTeam:
+    
+    def _init_(self, name):
+        self.name = name
+        
+        self.players = [] #This is a list of players on the team
+        self.player_sleeper_ids = [] #This is a list of player ids on the team
+        self.wins = 0
+        self.losses = 0
+        self.ties = 0  
+        
+        
+        
+        self.fantasy_pts_total = 0
+        self.fantasy_pts_per_game = 0
+        self.points_for = 0
+        self.points_against = 0
+        self.streak = 0
+
+        
---
+++ player.py
@@ -0,0 +1,55 @@
+class Player:
+    def __init__(self, name):
+        self.name = name
+        self.nfl_team = None
+        self.position = None
+        self.number = None
+        self.age = None
+        self.height = None
+        self.weight = None
+        self.college = None
+        self.years_exp = None
+        self.draft_year = None
+        self.depth_chart_position = None
+        self.status = None
+        self.sport = None
+        self.fantasy_positions = None
+        self.search_last_name = None
+        self.injury_start_date = None
+        self.practice_participation = None
+        self.sportradar_id = None
+        self.last_name = None
+        self.fantasy_data_id = None
+        self.injury_status = None
+        self.player_id = None
+        self.search_full_name = None
+        self.stats_id = None
+        self.birth_country = None
+        self.espn_id = None
+        self.search_rank = None
+        self.first_name = None
+        self.depth_chart_order = None
+        self.rotowire_id = None
+        self.rotoworld_id = None
+        self.search_first_name = None
+        self.yahoo_id = None
+        
+        self.current_fantasy_team = None
+        self.current_season_fantasy_pts = None
+        self.previous_season_fantasy_pts = None
+    
+        self.fantasycalc_rating_1QB = None
+        self.fantasycalc_rating_2QB = None
+        
+        self.fantasy_calc_ecr_1QB = None
+        self.fantasy_calc_ecr_2QB = None
+        
+        self.injury_risk = None
+        self.player_potential = None
+        
+        
+        
+        
+        
+    
+    
